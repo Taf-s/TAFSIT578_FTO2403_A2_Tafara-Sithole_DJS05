519fe38cfbc86853619d85a8ccb33d5cf076ba99
taf-s <tafara.sithole@gmail.com>
2024-07-25 23:58:04 +0200

    updated the reducer

---
+++ tally counter/reducer.js
@@ -0,0 +1,15 @@
+// Reducer
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { count: state.count + 1 };
+    case "SUBTRACT":
+      return { count: state.count - 1 };
+    case "RESET":
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
